ffd80a9121d23f26df289b8842fcf9e2550c5bfe
Meriem OUNI <elounimeriem@gmail.com>
2021-02-11 20:37:17 +0100

    first commit

---
+++ src/components/Info.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Button, Card} from 'react-bootstrap';
+import PropTypes from 'prop-types'; 
+const Info = (props) => {
+    return (
+        <div>
+            <div>
+                <Card className="card" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="/info.jpg" style={{width:"300px", height:"300px"}} />
+                <Card.Body>
+                    <Card.Title>{props.fullName}</Card.Title>
+                    <Card.Text>
+                    <h2>{props.bio}</h2>
+                    <h2>{props.profession}</h2>
+                    </Card.Text>
+                    <Button variant="primary"><a href="https://www.facebook.com/creativeartbymo" alt="facebook">Facebook Page</a></Button>
+                </Card.Body>
+                </Card>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Info
